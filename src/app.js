f552f2483379718384a2102ed4fc42f80487cd2e
Maria Planella Vall <mplanellav@uoc.edu>
2025-04-01 00:15:24 +0200

    Actualizaciones del codigo + estilos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    new Swiper(".swiper", {
+        modules: [Navigation, Pagination],
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
